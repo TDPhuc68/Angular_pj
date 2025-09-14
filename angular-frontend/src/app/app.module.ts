200be1baff8d93f2c1258ec09a90ad68bca6452d
TDPhuc68 <your_email@example.com>
2025-09-14 09:12:40 +0700

    frontend

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { FormsModule} from '@angular/forms';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { RouterModule } from '@angular/router'; 
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component'
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { EmployeeDetailsComponent } from './employee-details/employee-details.co
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule.forRoot([]),
     FormsModule
   ],
   providers: [],
